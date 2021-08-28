2e6d568ec862cdb0f4e3ce899f34198c7d6b8b41
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2021-08-27 22:51:33 -0700

    added guildMemberRemove event file

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+	name: 'guildMemberRemove',
+	async execute(member) {
+        const logChannel = member.guild.channels.cache.find(channel => channel.name === "spam")
+        if(!logChannel) return
+
+        const leaveEmbed = new MessageEmbed()
+            .setAuthor(member.displayName + ' has left the server.', member.user.displayAvatarURL({ dynamic : true}))
+            .addField('User ID: ', `${member.user.id}`, true)
+            .setColor(0xdf0000)
+            .setThumbnail(member.user.displayAvatarURL({ dynamic : true}))
+            .setFooter(member.guild.name, member.guild.iconURL({ dynamic : true}))
+            .setTimestamp()
+
+        logChannel.send({ embeds: [leaveEmbed] })
+	},
+}
