67f81f89461edcddd446a8d0d88c1cf79b4ef0f5
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2021-08-28 16:30:30 -0700

    added moderator role check

--- commands/say.js
+++ commands/say.js
@@ -15,14 +15,21 @@ module.exports = {
             .setRequired(true)),
 		
 	async execute(interaction) {
-        const targetChannel = interaction.options._hoistedOptions[0].channel
-        const messageToSend = interaction.options._hoistedOptions[1].value
-        targetChannel.send(messageToSend)
+        if(interaction.member.roles.cache.has('691882703674540042')) { // Moderator role
+            const targetChannel = interaction.options._hoistedOptions[0].channel
+            const messageToSend = interaction.options._hoistedOptions[1].value
+            targetChannel.send(messageToSend)
 
-        const sentEmbed = new MessageEmbed()
-            .setDescription(`Said **"${messageToSend}"** in ${targetChannel}`)
-            .setColor(0x32ff25)
+            const sentEmbed = new MessageEmbed()
+                .setDescription(`Said **"${messageToSend}"** in ${targetChannel}`)
+                .setColor(0x32ff25)
 
-        interaction.reply({ embeds: [sentEmbed] })
+            interaction.reply({ embeds: [sentEmbed] })
+        } else {
+            const permsEmbed = new MessageEmbed()
+                .setDescription('You do not have permission to use this command.')
+                .setColor(0xdf0000)
+            return interaction.reply({ embeds: [permsEmbed], ephemeral: true })
+        }
 	},
 }
