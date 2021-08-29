d576167398b7a2dfc8bf3c0c4426041c4677a3dc
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2021-08-28 17:35:30 -0700

    added serverinfo command

---
+++ commands/serverinfo.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('serverinfo')
+		.setDescription(`Get basic info about this server`),
+		
+	async execute(interaction) {
+        const createdAt = interaction.guild.createdAt.toString().split(' ')
+
+        const serverInfo = new MessageEmbed()
+            .setTitle(`***${interaction.guild}*** server info`)
+            .setDescription(interaction.guild.description)
+            .setThumbnail(interaction.guild.iconURL({ dynamic : true}))
+            .setColor(0xf03200)
+            .addFields(
+                {
+                    name: 'Owner',
+                    value: `<@${interaction.guild.ownerId}>`,
+                    inline: true,
+                },
+                {
+                    name: 'Date Created',
+                    value: `${createdAt[1]} ${createdAt[2]} ${createdAt[3]}`,
+                    inline: true,
+                },
+                {
+                    name: 'Member Count',
+                    value: `${interaction.guild.memberCount}`,
+                    inline: true,
+                },
+                {
+                    name: 'Server Level',
+                    value: `${interaction.guild.premiumTier}`.slice(5), // remove 'TIER_' from 'TIER_#'
+                    inline: true,
+                }, 
+                {
+                    name: 'Server Boosts',
+                    value: `${interaction.guild.premiumSubscriptionCount}`,
+                    inline: true,
+                },     
+            )
+
+		interaction.reply({ embeds: [serverInfo] })
+	},
+}
