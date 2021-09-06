7e7519012889271928cd36a75072f584abd99034
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2021-09-06 11:45:49 -0700

    added warn command

---
+++ commands/warn.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('warn')
+		.setDescription('Warn a user')
+        .addUserOption(option =>
+            option.setName('user')
+            .setDescription('The user to be warned')
+            .setRequired(true))
+        .addStringOption(option => 
+            option.setName('reason')
+            .setDescription('The reason for the warn')
+            .setRequired(true)),
+		
+	async execute(interaction) {
+        if(interaction.member.roles.cache.has('691882703674540042')) { // Moderator role
+            const userToWarn = interaction.options._hoistedOptions[0].user
+            const reasonForWarn = interaction.options._hoistedOptions[1].value
+
+            const modChannel = interaction.guild.channels.cache.find(channel => channel.name === "moderators")
+            if(!modChannel) return
+
+            const logEmbed = new MessageEmbed()
+                .setTitle(userToWarn.tag + ' was warned.')
+                .addField('User ID: ', `${userToWarn.id}`, true)        
+                .addField('By: ', `${interaction.user}`, true)
+                .addField('Reason: ', reasonForWarn)
+                .setColor(0xffd100)
+                .setThumbnail(userToWarn.displayAvatarURL({ dynamic : true }))
+                .setFooter(interaction.guild.name, interaction.guild.iconURL({ dynamic : true }))
+                .setTimestamp()
+            modChannel.send({ embeds: [logEmbed] })
+
+            const warnEmbed = new MessageEmbed()
+                .setTitle(`You were warned in **${interaction.guild.name}**.`)
+                .setDescription(reasonForWarn)
+                .setColor(0xffd100)
+                .setFooter(interaction.guild.name, interaction.guild.iconURL({ dynamic : true }))
+                .setTimestamp()
+            
+            try {
+                await userToWarn.send({ embeds: [warnEmbed] })
+            } catch(err) {
+                console.log(err)
+            }
+
+            const warnedEmbed = new MessageEmbed()
+                .setDescription(`${userToWarn} was warned.`)
+                .setColor(0xffd100)
+            interaction.reply({ embeds: [warnedEmbed] })
+        } else {
+            const permsEmbed = new MessageEmbed()
+                .setDescription('You do not have permission to use this command.')
+                .setColor(0xdf0000)
+            return interaction.reply({ embeds: [permsEmbed], ephemeral: true })
+        }
+	},
+}
