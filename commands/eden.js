1f589121aad0d91c16064443d48c26737048534e
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2021-09-06 13:02:46 -0700

    added eden command

---
+++ commands/eden.js
@@ -0,0 +1,50 @@
+var imgur = require('imgur')
+
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('eden')
+		.setDescription('Get a random picture of EDEN'),
+		
+	async execute(interaction) {
+        var edenAlbumOne = '3Zh414x'
+        var edenAlbumTwo = 'DZ913Hd'
+        var edenAlbumThree = 'PUfyYtt'
+
+        var edenImages = []
+
+        imgur.getAlbumInfo(edenAlbumOne)
+            .then((json) => {
+                json.images.forEach(image => {
+                    edenImages.push(image.link)
+                })
+            })
+            .catch((err) => {
+                console.error(err.message)
+            })
+
+        imgur.getAlbumInfo(edenAlbumTwo)
+            .then((json) => {
+                json.images.forEach(image => {
+                    edenImages.push(image.link)
+                })
+            })
+        .catch((err) => {
+            console.error(err.message)
+        })
+
+        imgur.getAlbumInfo(edenAlbumThree)
+            .then((json) => {
+                json.images.forEach(image => {
+                    edenImages.push(image.link)
+                })
+
+                interaction.reply({ content: `${edenImages[Math.floor(Math.random()*(edenImages.length))]}` })
+            })
+            .catch((err) => {
+                console.error(err.message)
+            })
+	},
+}
