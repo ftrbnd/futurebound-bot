de5d71fee9b73532d622cf0dac090329749ecf3f
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2021-09-06 14:11:35 -0700

    added channelUpdate event

---
+++ events/channelUpdate.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+	name: 'channelUpdate',
+	async execute(oldChannel, newChannel) {
+        const logChannel = oldChannel.guild.channels.cache.find(channel => channel.name === "spam")
+		if(!logChannel) return
+			
+        const channelType = (oldChannel.type === 'GUILD_TEXT') ? "text" : "voice" // if oldChannel type is GUILD_TEXT, then set channelType to text
+
+        if(oldChannel.name != newChannel.name) {
+            const changedEmbed = new MessageEmbed()
+                .setTitle(`A ${channelType} channel's name was changed.`)
+                .addFields(
+                    { name: 'Previous name', value: oldChannel.name, inline: true },
+                    { name: 'New name', value: newChannel.name, inline: true },
+                )
+                .setColor(0x32ff25)
+                .setFooter(`${oldChannel.guild.name}`, oldChannel.guild.iconURL({ dynamic: true }))
+                .setTimestamp()
+            logChannel.send({ embeds: [changedEmbed] })
+        }
+	},
+}
