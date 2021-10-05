1a06d6a21271b81578d5318efdf56e4667b9bb68
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2021-10-04 23:21:10 -0700

    added channel partial

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const fs = require('fs')
 // Discord
 
 const { Client, Collection, Intents, MessageEmbed} = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.DIRECT_MESSAGES] })
+const client = new Client({ 
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.DIRECT_MESSAGES],
+    partials: ['CHANNEL']
+})
 
 client.commands = new Collection()
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))  // Command handler
