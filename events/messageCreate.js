a1a6941f020a3027ac5448f1db78783288fe5744
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2021-10-23 11:41:48 -0700

    ignore bot messages

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,6 +3,7 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
 	name: 'messageCreate',
 	async execute(message) {
+        if(message.author.bot) return // ignore bot messages
 
         if(message.channel.type === 'DM') return
 
