a598b2b472a7798598b184b66466c3440033089b
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2022-06-01 21:01:14 -0700

    now listening to Modern Warfare

--- events/ready.js
+++ events/ready.js
@@ -6,15 +6,16 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client) {
-        const songs = ['02:09', 'End Credits', 'Gravity', 'Nocturne', 'Interlude', 'Wake Up', 
-        'catch me if you can', 'Billie Jean', 'sex', 'drugs', 'and', 'rock + roll', 
-        'Fumes', 'XO', 'Circles', 'wrong', 'take care', 'start//end', 'wings', 'icarus', 
-        'lost//found', 'crash', 'gold', 'forever//over', 'float', 'wonder', 
-        'love; not wrong (brave)', 'falling in reverse', 'about time', 'stutter', 'all you need is love', 
-        'nowhere else', '909', 'good morning', 'in', 'hertz', 'static', 'projector', 
-        'love, death, distraction', 'how to sleep', 'calm down', 'just saying', 'fomo', 
-        'so far so good', 'isohel', 'tides', 'rushing', '$treams', '2020', 'out', 'untitled', 
-        'Peaked', 'Cold Feet', 'Stingray', 'cant help', '🔒 (demo)']
+        // const songs = ['02:09', 'End Credits', 'Gravity', 'Nocturne', 'Interlude', 'Wake Up', 
+        // 'catch me if you can', 'Billie Jean', 'sex', 'drugs', 'and', 'rock + roll', 
+        // 'Fumes', 'XO', 'Circles', 'wrong', 'take care', 'start//end', 'wings', 'icarus', 
+        // 'lost//found', 'crash', 'gold', 'forever//over', 'float', 'wonder', 
+        // 'love; not wrong (brave)', 'falling in reverse', 'about time', 'stutter', 'all you need is love', 
+        // 'nowhere else', '909', 'good morning', 'in', 'hertz', 'static', 'projector', 
+        // 'love, death, distraction', 'how to sleep', 'calm down', 'just saying', 'fomo', 
+        // 'so far so good', 'isohel', 'tides', 'rushing', '$treams', '2020', 'out', 'untitled', 
+        // 'Peaked', 'Cold Feet', 'Stingray', 'cant help', '🔒 (demo)', 'Modern Warfare']
+        const songs = ['Modern Warfare']
 
         var updateDelay = 180
         let currentIndex = 0
