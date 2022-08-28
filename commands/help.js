beabfa58ffc95ef4ee140ab68a0484cda66dc8e4
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2022-08-27 18:40:10 -0700

    added fields for different roles

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+const { EmbedBuilder, SlashCommandBuilder, ActivityFlagsBitField } = require('discord.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,21 +8,45 @@ module.exports = {
 		.setDescription('Get a list of the commands for this bot'),
 		
 	async execute(interaction) {
-		var commandList = ''
-		const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-		for (const file of commandFiles) {
-			commandList += `${file.replace('.js', '')}\n`
-		}
-
-		const helpEmbed = new EmbedBuilder()
+		var helpEmbed = new EmbedBuilder()
 			.setTitle(`Commands for ***${interaction.client.user.tag}***`)
-			.setDescription(commandList)
 			.setFooter({
 				text: interaction.guild.name, 
 				iconURL: interaction.guild.iconURL({ dynamic : true})
 			})
 			.setColor('0xf03200')
+		
+		var everyoneCommandsList = ''
+		var helperCommandsList = ''
+		var modCommandsList = ''
+		const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+		for (const file of commandFiles) {
+			const command = require(`./${file}`)
 
+			if (!command.data.default_member_permissions) { // if the command has no Helper/Mod permissions required
+				everyoneCommandsList += `${file.replace('.js', '')}\n`
+			} else if (command.data.default_member_permissions === '268435456') { // Helper command
+				helperCommandsList += `${file.replace('.js', '')}\n`
+			} else if (command.data.default_member_permissions === '8') { // Moderator command
+				modCommandsList += `${file.replace('.js', '')}\n`
+			}
+		}
+
+		if(interaction.member.roles.cache.has(process.env.MODERATORS_ROLE_ID)) { // Moderator role
+			helpEmbed.addFields([
+				{ name: 'Moderator Commands', value: modCommandsList },
+				{ name: 'Helper Commands', value: helperCommandsList },
+				{ name: '@everyone Commands', value: everyoneCommandsList }
+			])
+		} else if(interaction.member.roles.cache.has(process.env.HELPER_ROLE_ID)) { // Helper role
+			helpEmbed.addFields([
+				{ name: 'Helper Commands', value: helperCommandsList },
+				{ name: '@everyone Commands', value: everyoneCommandsList }
+			])
+		} else { // @everyone
+			helpEmbed.setDescription(everyoneCommandsList)
+		}
+		
 		interaction.reply({ embeds: [helpEmbed] })
 	},
 }
