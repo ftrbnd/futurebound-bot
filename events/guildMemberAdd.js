88899b166350483abae9c341bd004864dd1cb28c
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2022-09-01 17:03:52 -0700

    updated description to not include the id directly

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,6 +5,8 @@ module.exports = {
 	async execute(member) {        
         const welcomeChannel = member.guild.channels.cache.get(process.env.WELCOME_CHANNEL_ID)
         if(!welcomeChannel) return
+        const rolesChannel = member.guild.channels.cache.get(process.env.ROLES_CHANNEL_ID)
+        if(!rolesChannel) return
         
         const welcomeEmbed = new EmbedBuilder()
             .setAuthor({
@@ -13,7 +15,7 @@ module.exports = {
             })
             .setColor('0x32ff25')
             .setThumbnail(member.user.displayAvatarURL({ dynamic : true}))
-            .setDescription(`Go to <#702231983853666335> to pick your favorite EP/album, and a color will be added to your name.`)
+            .setDescription(`Go to ${rolesChannel} to pick your favorite EP/album, and a color will be added to your name.`)
             .setFooter({
                 text: member.guild.name, 
                 iconURL: member.guild.iconURL({ dynamic : true})
