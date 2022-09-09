b5b1ac07521e1efdcfef1c7690d88d22c18832ca
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2022-09-08 17:43:13 -0700

    uncommented reddit watcher

--- index.js
+++ index.js
@@ -67,52 +67,52 @@ stream.on('tweet', tweet => {
 
 // Reddit
 
-// var Snooper = require('reddit-snooper')
+var Snooper = require('reddit-snooper')
 
-// snooper = new Snooper(
-//     {
-//         app_id: process.env.APP_ID,
-//         api_secret: process.env.API_SECRET,
+const snooper = new Snooper(
+    {
+        app_id: process.env.APP_ID,
+        api_secret: process.env.API_SECRET,
 
-//         automatic_retries: true,
-//         api_requests_per_minute: 60
-//     }
-// )
+        automatic_retries: true,
+        api_requests_per_minute: 60
+    }
+)
 
-// snooper.watcher.getPostWatcher('eden') // blank argument or 'all' looks at the entire website
-//     .on('post', function(post) {
-//         const subredditChannel = client.channels.cache.get(process.env.SUBREDDIT_CHANNEL_ID)
+snooper.watcher.getPostWatcher('pics') // blank argument or 'all' looks at the entire website
+    .on('post', function(post) {
+        const subredditChannel = client.channels.cache.get(process.env.SUBREDDIT_CHANNEL_ID)
         
-//         // console.log(post)
+        // console.log(post)
 
-//         var redditEmbed = new EmbedBuilder()
-//             .setTitle(post.data.title.substring(0, 255))
-//             .setURL(`https://reddit.com${post.data.permalink}`)
-//             .setImage(post.data.url)
-//             .setColor('0xFF4500')
-//             .setFooter({ 
-//                 text: `Posted by u/${post.data.author} on r/${post.data.subreddit}`,
-//                 iconURL: 'https://logodownload.org/wp-content/uploads/2018/02/reddit-logo-16.png'
-//             })
-//             .setTimestamp()
+        var redditEmbed = new EmbedBuilder()
+            .setTitle(post.data.title.substring(0, 255))
+            .setURL(`https://reddit.com${post.data.permalink}`)
+            .setImage(post.data.url)
+            .setColor('0xFF4500')
+            .setFooter({ 
+                text: `Posted by u/${post.data.author} on r/${post.data.subreddit}`,
+                iconURL: 'https://logodownload.org/wp-content/uploads/2018/02/reddit-logo-16.png'
+            })
+            .setTimestamp()
 
-//         if (post.data.selftext !== '')
-//             redditEmbed.setDescription(post.data.selftext)
+        if (post.data.selftext !== '')
+            redditEmbed.setDescription(post.data.selftext)
 
-//         subredditChannel.send({ embeds: [redditEmbed] })
-//             .then(() => subredditChannel.messages.fetch({ limit: 1 }) // fetch latest message
-//                 .then(messages => {
-//                     let lastMessage = messages.first() // message retrieved
-//                     const upvoteEmoji = client.emojis.cache.get(process.env.UPVOTE_EMOJI_ID)
-//                     const downvoteEmoji = client.emojis.cache.get(process.env.DOWNVOTE_EMOJI_ID)
+        subredditChannel.send({ embeds: [redditEmbed] })
+            .then(() => subredditChannel.messages.fetch({ limit: 1 }) // fetch latest message
+                .then(messages => {
+                    let lastMessage = messages.first() // message retrieved
+                    const upvoteEmoji = client.emojis.cache.get(process.env.UPVOTE_EMOJI_ID)
+                    const downvoteEmoji = client.emojis.cache.get(process.env.DOWNVOTE_EMOJI_ID)
 
-//                     lastMessage.react(upvoteEmoji)     // react with upvote
-//                         .then(() => lastMessage.react(downvoteEmoji)) // react with downvote
-//                 })
-//                 .catch(console.error))
+                    lastMessage.react(upvoteEmoji)     // react with upvote
+                        .then(() => lastMessage.react(downvoteEmoji)) // react with downvote
+                })
+                .catch(console.error))
 
-//     })
-//     .on('error', console.error)
+    })
+    .on('error', console.error)
 
 // Mongo DB
 
