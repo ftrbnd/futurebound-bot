e08f0b0a4bd43c8561b9e21b63853d2ec7603b0d
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2022-09-22 21:27:18 -0700

    added attachments

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -9,24 +9,37 @@ module.exports = {
 		if(!logChannel) return
 		if(message.author.bot) return
 
-		var content
-		if(!message.content || message.content === '') {
-			content = ' '
-		} else {
-			content = message.content
-		}
-			
 		const msgDeleteEmbed = new EmbedBuilder()
 			.setAuthor({
-				name: `A message by ${message.author.tag} was deleted.`, 
+				name: `A message by ${message.author.tag} was deleted`, 
 				iconURL: message.author.displayAvatarURL({ dynamic : true })
 			})
-			.setDescription(content)
 			.addFields([
 				{ name: 'Channel', value: `${message.channel}`},
 			])
 			.setColor('0xdf0000')
 			.setTimestamp()
-		logChannel.send({ embeds: [msgDeleteEmbed] })
+
+		if(message.attachments.size > 0) {
+			let messageNote = '**Attachment(s) will be sent below**'
+			if(message.content != '') // if the message had any text
+				messageNote = '**Text:** ' + message.content + `\n${messageNote}`
+
+			msgDeleteEmbed.setDescription(messageNote)
+			const embedMessage = await logChannel.send({ embeds: [msgDeleteEmbed] })
+			
+			message.attachments.forEach(async attachment => {
+				const attachmentEmbed = new EmbedBuilder()
+					.setTitle(`Deleted attachment from ${message.author.tag}`)
+					.setImage(attachment.url)
+					.setColor('0xdf0000')
+
+				await embedMessage.reply({ embeds: [attachmentEmbed] })
+			})
+
+		} else if(message.content != '' && message.attachments.size == 0) { // message has text only and no attachments
+			msgDeleteEmbed.setDescription('**Text:** ' + message.content)
+			logChannel.send({ embeds: [msgDeleteEmbed] })
+		}
 	},
 }
