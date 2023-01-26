e3af80a93ba392e779e0248d64fe987b8c4cd11f
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-25 20:56:49 -0800

    Clean up code

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,8 +1,7 @@
-require('dotenv').config()
-const fs = require('fs')
-const path = require('path')
-
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -14,82 +13,77 @@ module.exports = {
             .setRequired(true)),
 		
 	async execute(interaction) {
-        var song = interaction.options.getString('song').toLowerCase()
+        let song = interaction.options.getString('song').toLowerCase();
 
-        const lyricsFolder = path.resolve(__dirname, '../lyrics')
-        const songFiles = fs.readdirSync(lyricsFolder).filter(file => file.endsWith('.txt'))
+        const lyricsFolder = path.resolve(__dirname, '../lyrics');
+        const songFiles = fs.readdirSync(lyricsFolder).filter(file => file.endsWith('.txt'));
 
         for(let i = 0; i < songFiles.length; i++) {
-            let songName = songFiles[i].slice(0, -4) // remove '.txt'
+            let songName = songFiles[i].slice(0, -4); // remove '.txt'
             switch(songName) { // handle ---- to ????, start--end to start//end, etc.
                 case "----":
-                    songName = "????"
-                    break
+                    songName = "????";
+                    break;
                 case "start--end":
-                    songName = "start//end"
-                    break
+                    songName = "start//end";
+                    break;
                 case "lost--found":
-                    songName = "lost//found"
-                    break
+                    songName = "lost//found";
+                    break;
                 case "forever--over":
-                    songName = "forever//over"
-                    break
-                default:
-                    // do nothing
+                    songName = "forever//over";
+                    break;
             }
 
             if(song === songName.toLowerCase()) { // if the song the user requested and the current song are the same
-                const lyrics = await readFile(`${lyricsFolder}/${songFiles[i]}`) // get the lyrics
-                const lyricsString = lyrics.join('\n')
-                song = songName
+                const lyrics = await readFile(`${lyricsFolder}/${songFiles[i]}`); // get the lyrics
+                const lyricsString = lyrics.join('\n');
+                song = songName;
 
-                if(songName.toLowerCase() === "Fumes".toLowerCase()) {
-                    songName = "Fumes (feat. gnash)"
-                }
+                if(songName.toLowerCase() === "Fumes".toLowerCase())
+                    songName = "Fumes (feat. gnash)";
 
                 let lyricsEmbed = new EmbedBuilder()
                     .setTitle(songName)
                     .setDescription(lyricsString)
-                    .setColor('0xdf0000')
+                    .setColor('0xdf0000');
 
-                const albumsFolder = path.resolve(__dirname, '../albums')
-                const albumFiles = fs.readdirSync(albumsFolder).filter(file => file.endsWith('.txt'))
+                const albumsFolder = path.resolve(__dirname, '../albums');
+                const albumFiles = fs.readdirSync(albumsFolder).filter(file => file.endsWith('.txt'));
                 for(let i = 0; i < albumFiles.length; i++) { // check if the song belongs to any album
-                    let album = albumFiles[i]
-
-                    var albumTracks = await readFile(`${albumsFolder}/${albumFiles[i]}`) // get the album tracks
-                    const embedColor = `0x${albumTracks.pop()}`
-                    const albumCover = albumTracks.pop()
+                    const albumTracks = await readFile(`${albumsFolder}/${albumFiles[i]}`);
+                    const embedColor = `0x${albumTracks.pop()}`;
+                    const albumCover = albumTracks.pop();
                     if(albumTracks.includes(songName)) {
-                        lyricsEmbed.setColor(embedColor)
-                        lyricsEmbed.setThumbnail(albumCover)
-                        break // exit out of this loop once we find the album
+                        lyricsEmbed.setColor(embedColor);
+                        lyricsEmbed.setThumbnail(albumCover);
+                        break;
                     } else { // if the song is not from any album
-                        lyricsEmbed.setColor('Grey')
-                        lyricsEmbed.setThumbnail('https://i.imgur.com/rQmm1FM.png')
+                        lyricsEmbed.setColor('Grey');
+                        lyricsEmbed.setThumbnail('https://i.imgur.com/rQmm1FM.png');
                     }
                 }
 
-                return interaction.reply({ embeds: [lyricsEmbed] })
+                return interaction.reply({ embeds: [lyricsEmbed] });
             }
         }
 
         if(!songFiles.includes(song)) {
             const errEmbed = new EmbedBuilder()
                 .setDescription(`**${song}** is not a valid song, please try again!`)
-                .setColor('0xdf0000')
-            return interaction.reply({ embeds: [errEmbed], ephemeral: true })
+                .setColor('0xdf0000');
+            return interaction.reply({ embeds: [errEmbed], ephemeral: true });
         }
 	},
 }
 
 async function readFile(filename) {
     try {
-        const contents = await fs.promises.readFile(filename, 'utf-8')
-        const arr = contents.split(/\r?\n/)
+        const contents = await fs.promises.readFile(filename, 'utf-8');
+        const arr = contents.split(/\r?\n/);
 
-        return arr
+        return arr;
     } catch(err) {
-        console.log(err)
+        console.error(err);
     }
 }
