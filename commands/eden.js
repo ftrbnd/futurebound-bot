e6a747947fb6215c2a7b249c518a823a215539fe
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-25 20:16:54 -0800

    Clean up code

--- commands/eden.js
+++ commands/eden.js
@@ -1,6 +1,5 @@
-var imgur = require('imgur')
-
-const { SlashCommandBuilder } = require('discord.js')
+const imgur = require('imgur');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,42 +7,41 @@ module.exports = {
 		.setDescription('Get a random picture of EDEN'),
 		
 	async execute(interaction) {
-        var edenAlbumOne = '3Zh414x'
-        var edenAlbumTwo = 'DZ913Hd'
-        var edenAlbumThree = 'PUfyYtt'
-
-        var edenImages = []
+        const edenAlbumOne = '3Zh414x';
+        const edenAlbumTwo = 'DZ913Hd';
+        const edenAlbumThree = 'PUfyYtt';
+        const edenImages = [];
 
         imgur.getAlbumInfo(edenAlbumOne)
             .then((json) => {
                 json.images.forEach(image => {
-                    edenImages.push(image.link)
-                })
+                    edenImages.push(image.link);
+                });
             })
             .catch((err) => {
-                console.error(err.message)
-            })
+                console.error(err.message);
+            });
 
         imgur.getAlbumInfo(edenAlbumTwo)
             .then((json) => {
                 json.images.forEach(image => {
-                    edenImages.push(image.link)
-                })
+                    edenImages.push(image.link);
+                });
             })
-        .catch((err) => {
-            console.error(err.message)
-        })
+            .catch((err) => {
+                console.error(err.message);
+            });
 
         imgur.getAlbumInfo(edenAlbumThree)
             .then((json) => {
                 json.images.forEach(image => {
-                    edenImages.push(image.link)
-                })
+                    edenImages.push(image.link);
+                });
 
-                interaction.reply({ files: [`${edenImages[Math.floor(Math.random()*(edenImages.length))]}`] })
+                interaction.reply({ files: [`${edenImages[Math.floor(Math.random()*(edenImages.length))]}`] });
             })
             .catch((err) => {
-                console.error(err.message)
-            })
+                console.error(err.message);
+            });
 	},
 }
