99752318379604fa1b4c1ab25339b2c754318646
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-25 20:37:19 -0800

    Clean up code

--- commands/kick.js
+++ commands/kick.js
@@ -1,6 +1,5 @@
-require('dotenv').config()
-
-const { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } = require('discord.js')
+require('dotenv').config();
+const { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -17,61 +16,53 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),  // only the Server Moderator role can use this command
 		
 	async execute(interaction) {
-        if(interaction.member.roles.cache.has(process.env.MODERATORS_ROLE_ID)) { // Moderator role
-            const userToKick = interaction.options.getUser('user')
-            const reasonForKick = interaction.options.getString('reason')
+        const userToKick = interaction.options.getUser('user');
+        const reasonForKick = interaction.options.getString('reason');
 
-            const modChannel = interaction.guild.channels.cache.get(process.env.MODERATORS_CHANNEL_ID)
-            if(!modChannel) return
+        const modChannel = interaction.guild.channels.cache.get(process.env.MODERATORS_CHANNEL_ID);
+        if(!modChannel) return;
 
-            try {
-                interaction.guild.members.kick(userToKick, options = { reason: reasonForKick})
-            } catch(err) {
-                return console.log(err)
-            }
-
-            const logEmbed = new EmbedBuilder()
-                .setTitle(userToKick.tag + ' was kicked.')
-                .addFields([
-                    { name: 'User ID: ', value: `${userToKick.id}`},
-                    { name: 'By: ', value: `${interaction.user}`},
-                    { name: 'Reason: ', value: reasonForKick},
-                ])
-                .setColor('0xdf0000')
-                .setThumbnail(userToKick.displayAvatarURL({ dynamic : true }))
-                .setFooter({
-                    text: interaction.guild.name, 
-                    iconURL: interaction.guild.iconURL({ dynamic : true })
-                })
-                .setTimestamp()
-            modChannel.send({ embeds: [logEmbed] })
-
-            const kickEmbed = new EmbedBuilder()
-                .setTitle(`You were kicked from **${interaction.guild.name}**.`)
-                .setDescription(reasonForKick)
-                .setColor('0xdf0000')
-                .setFooter({
-                    text: interaction.guild.name, 
-                    iconURL: interaction.guild.iconURL({ dynamic : true })
-                })
-                .setTimestamp()
-            
-            try {
-                await userToKick.send({ embeds: [kickEmbed] })
-            } catch(err) {
-                return console.log(err)
-            }
-                
-            const kickedEmbed = new EmbedBuilder()
-                .setDescription(`${userToKick} was kicked.`)
-                .setColor('0x32ff25')
-            interaction.reply({ embeds: [kickedEmbed] })
-
-        } else {
-            const permsEmbed = new EmbedBuilder()
-                .setDescription('You do not have permission to use this command.')
-                .setColor('0xdf0000')
-            return interaction.reply({ embeds: [permsEmbed], ephemeral: true })
+        try {
+            interaction.guild.members.kick(userToKick, options = { reason: reasonForKick});
+        } catch(err) {
+            return console.log(err);
         }
-	},
+
+        const logEmbed = new EmbedBuilder()
+            .setTitle(userToKick.tag + ' was kicked.')
+            .addFields([
+                { name: 'User ID: ', value: `${userToKick.id}`},
+                { name: 'By: ', value: `${interaction.user}`},
+                { name: 'Reason: ', value: reasonForKick},
+            ])
+            .setColor('0xdf0000')
+            .setThumbnail(userToKick.displayAvatarURL({ dynamic : true }))
+            .setFooter({
+                text: interaction.guild.name, 
+                iconURL: interaction.guild.iconURL({ dynamic : true })
+            })
+            .setTimestamp();
+        modChannel.send({ embeds: [logEmbed] });
+
+        const kickEmbed = new EmbedBuilder()
+            .setTitle(`You were kicked from **${interaction.guild.name}**.`)
+            .setDescription(reasonForKick)
+            .setColor('0xdf0000')
+            .setFooter({
+                text: interaction.guild.name, 
+                iconURL: interaction.guild.iconURL({ dynamic : true })
+            })
+            .setTimestamp();
+        
+        try {
+            await userToKick.send({ embeds: [kickEmbed] });
+        } catch(err) {
+            return console.error(err);
+        }
+            
+        const kickedEmbed = new EmbedBuilder()
+            .setDescription(`${userToKick} was kicked.`)
+            .setColor('0x32ff25');
+        interaction.reply({ embeds: [kickedEmbed] });
+    }
 }
