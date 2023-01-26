f06f9014091ea830180e3e29aad3a8061fa38458
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-25 21:03:09 -0800

    Clean up code

--- commands/react.js
+++ commands/react.js
@@ -1,6 +1,5 @@
-require('dotenv').config()
-
-const { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder, ChannelType } = require('discord.js')
+require('dotenv').config();
+const { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -18,29 +17,22 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),  // only the Server Moderator role can use this command
         
 	async execute(interaction) {
-        if(interaction.member.roles.cache.has(process.env.MODERATORS_ROLE_ID)) { // Moderator role
-            const channel = interaction.options.getChannel('channel')
-            const emoji = interaction.options.getString('emoji')
+        const channel = interaction.options.getChannel('channel');
+        const emoji = interaction.options.getString('emoji');
 
-            try {
-                channel.lastMessage.react(emoji)
+        try {
+            channel.lastMessage.react(emoji);
 
-                const confirmEmbed = new EmbedBuilder()
-                    .setDescription(`Reacted to ${channel.lastMessage} with ${emoji}`)
-                    .setColor('0x32ff25')
-                interaction.reply({ embeds: [confirmEmbed], ephemeral: true })
-            } catch(error) {
-                console.log(error)
-                const errorEmbed = new EmbedBuilder()
-                    .setDescription(`Failed to react with ${emoji}`)
-                    .setColor('0xdf0000')
-                interaction.reply({ embeds: [errorEmbed], ephemeral: true })
-            }
-        } else {
-            const permsEmbed = new EmbedBuilder()
-                .setDescription('You do not have permission to use this command.')
-                .setColor('0xdf0000')
-            return interaction.reply({ embeds: [permsEmbed], ephemeral: true })
+            const confirmEmbed = new EmbedBuilder()
+                .setDescription(`Reacted to ${channel.lastMessage} with ${emoji}`)
+                .setColor('0x32ff25');
+            interaction.reply({ embeds: [confirmEmbed], ephemeral: true });
+        } catch(err) {
+            console.error(err);
+            const errorEmbed = new EmbedBuilder()
+                .setDescription(`Failed to react with ${emoji}`)
+                .setColor('0xdf0000');
+            interaction.reply({ embeds: [errorEmbed], ephemeral: true });
         }
 	},
 }
