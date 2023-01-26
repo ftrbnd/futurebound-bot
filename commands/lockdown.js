611a4f4860ecfbe6202748907d9905803205eebb
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-25 20:51:17 -0800

    Clean up code

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,7 +1,6 @@
-require('dotenv').config()
-
-const { PermissionFlagsBits } = require('discord-api-types/v9')
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+require('dotenv').config();
+const { PermissionFlagsBits } = require('discord-api-types/v9');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -17,48 +16,44 @@ module.exports = {
                 .setDescription('Re-open all text channels'))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles), // any permission that the Helper role has access to should work
 		
-	async execute(interaction) {
-        if(interaction.member.roles.cache.has(process.env.MODERATORS_ROLE_ID) || interaction.member.roles.cache.has(process.env.HELPER_ROLE_ID)) { // Moderator and Helper roles
-            
-            const roles = [
-                interaction.guild.roles.cache.get('704966097434312766'), // server boosters
-                interaction.guild.roles.cache.get('702225844113899591'), // no future
-                interaction.guild.roles.cache.get('702225672147566642'), // vertigo
-                interaction.guild.roles.cache.get('702226305164509185'), // ityttmom
-                interaction.guild.roles.cache.get('702226350324318299'), // End Credits
-                interaction.guild.roles.cache.get('655655072885374987')] // @everyone
+	async execute(interaction) {    
+        const roles = [
+            interaction.guild.roles.cache.get('704966097434312766'), // server boosters
+            interaction.guild.roles.cache.get('702225844113899591'), // no future
+            interaction.guild.roles.cache.get('702225672147566642'), // vertigo
+            interaction.guild.roles.cache.get('702226305164509185'), // ityttmom
+            interaction.guild.roles.cache.get('702226350324318299'), // End Credits
+            interaction.guild.roles.cache.get('655655072885374987')]; // @everyone
 
-            if(interaction.options.getSubcommand() === 'close') { // Close all text channels
+        if(interaction.options.getSubcommand() === 'close') { // Close all text channels
+            const removedPermissions = [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.CreateInstantInvite, 
+                PermissionFlagsBits.ChangeNickname, PermissionFlagsBits.AddReactions, PermissionFlagsBits.UseExternalEmojis, 
+                PermissionFlagsBits.UseExternalStickers, PermissionFlagsBits.ReadMessageHistory, PermissionFlagsBits.Connect, 
+                PermissionFlagsBits.Speak, PermissionFlagsBits.Stream, PermissionFlagsBits.UseVAD, PermissionFlagsBits.RequestToSpeak];
 
-                const removedPermissions = [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.CreateInstantInvite, PermissionFlagsBits.ChangeNickname, PermissionFlagsBits.AddReactions, PermissionFlagsBits.UseExternalEmojis, PermissionFlagsBits.UseExternalStickers, PermissionFlagsBits.ReadMessageHistory, PermissionFlagsBits.Connect, PermissionFlagsBits.Speak, PermissionFlagsBits.Stream, PermissionFlagsBits.UseVAD, PermissionFlagsBits.RequestToSpeak]
+            roles.forEach(role => role.setPermissions(removedPermissions));
 
-                roles.forEach(role => role.setPermissions(removedPermissions))
+            const confirmEmbed = new EmbedBuilder()
+                .setDescription(`**${interaction.guild.name}** is now on lockdown.`)
+                .setColor('0xdf0000');
+            interaction.reply({ embeds: [confirmEmbed] });
 
-                const confirmEmbed = new EmbedBuilder()
-                    .setDescription(`**${interaction.guild.name}** is now on lockdown.`)
-                    .setColor('0xdf0000')
-                return interaction.reply({ embeds: [confirmEmbed], ephemeral: false })
+        } else if(interaction.options.getSubcommand() === 'open') { // Open all text channels
+            const defaultPermissions = [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.CreateInstantInvite, 
+                PermissionFlagsBits.ChangeNickname, PermissionFlagsBits.SendMessages, PermissionFlagsBits.UsePublicThreads, 
+                PermissionFlagsBits.EmbedLinks, PermissionFlagsBits.AttachFiles, PermissionFlagsBits.AddReactions, 
+                PermissionFlagsBits.UseExternalEmojis, PermissionFlagsBits.UseExternalStickers, PermissionFlagsBits.ReadMessageHistory, 
+                PermissionFlagsBits.UseSlashCommands, PermissionFlagsBits.Connect, PermissionFlagsBits.Speak, PermissionFlagsBits.Stream, 
+                PermissionFlagsBits.UseVAD, PermissionFlagsBits.RequestToSpeak];
 
-            } else if(interaction.options.getSubcommand() === 'open') { // Open all text channels
-                
-                const defaultPermissions = [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.CreateInstantInvite, PermissionFlagsBits.ChangeNickname, PermissionFlagsBits.SendMessages, PermissionFlagsBits.UsePublicThreads, PermissionFlagsBits.EmbedLinks, PermissionFlagsBits.AttachFiles, PermissionFlagsBits.AddReactions, PermissionFlagsBits.UseExternalEmojis, PermissionFlagsBits.UseExternalStickers, PermissionFlagsBits.ReadMessageHistory, PermissionFlagsBits.UseSlashCommands, PermissionFlagsBits.Connect, PermissionFlagsBits.Speak, PermissionFlagsBits.Stream, PermissionFlagsBits.UseVAD, PermissionFlagsBits.RequestToSpeak]
+            roles.forEach(role => {
+                role.setPermissions(defaultPermissions);
+            });
 
-                roles.forEach(role => {
-                    role.setPermissions(defaultPermissions)
-                    // TODO: add SEND_MESSAGES_IN_THREADS permission
-                })
-
-                const confirmEmbed = new EmbedBuilder()
-                    .setDescription(`**${interaction.guild.name}** is now open!`)
-                    .setColor('0x32ff25')
-                return interaction.reply({ embeds: [confirmEmbed], ephemeral: false })
-
-            }
-        } else {
-            const permsEmbed = new EmbedBuilder()
-                .setDescription('You do not have permission to use this command.')
-                .setColor('0xdf0000')
-            return interaction.reply({ embeds: [permsEmbed], ephemeral: true })
+            const confirmEmbed = new EmbedBuilder()
+                .setDescription(`**${interaction.guild.name}** is now open!`)
+                .setColor('0x32ff25');
+            interaction.reply({ embeds: [confirmEmbed] });
         }
 	},
 }
