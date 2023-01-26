a98b07f90aff2ac9339deeebc10fbc5ac9255879
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-01-25 19:52:01 -0800

    Clean up code

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,5 @@
-require('dotenv').config()
-
-const { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } = require('discord.js')
+require('dotenv').config();
+const { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -17,61 +16,53 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),  // only the Server Moderator role can use this command
 		
 	async execute(interaction) {
-        if(interaction.member.roles.cache.has(process.env.MODERATORS_ROLE_ID)) { // Moderator role
-            const userToBan = interaction.options.getUser('user')
-            const reasonForBan = interaction.options.getString('reason')
+        const userToBan = interaction.options.getUser('user');
+        const reasonForBan = interaction.options.getString('reason');
 
-            const modChannel = interaction.guild.channels.cache.get(process.env.MODERATORS_CHANNEL_ID)
-            if(!modChannel) return
+        const modChannel = interaction.guild.channels.cache.get(process.env.MODERATORS_CHANNEL_ID);
+        if(!modChannel) return;
 
-            try {
-                await interaction.guild.members.ban(userToBan, options = { reason: reasonForBan})
-            } catch(err) {
-                return console.log(err)
-            }
-
-            const logEmbed = new EmbedBuilder()
-                .setTitle(userToBan.tag + ' was banned.')
-                .addFields([
-                    { name: 'User ID: ', value: `${userToBan.id}`},
-                    { name: 'By: ', value: `${interaction.user}`},
-                    { name: 'Reason: ', value: reasonForBan},
-                ])
-                .setColor('0xdf0000')
-                .setThumbnail(userToBan.displayAvatarURL({ dynamic : true }))
-                .setFooter({
-                    text: interaction.guild.name, 
-                    iconURL: interaction.guild.iconURL({ dynamic : true })
-                })
-                .setTimestamp()
-            modChannel.send({ embeds: [logEmbed] })
-
-            const banEmbed = new EmbedBuilder()
-                .setTitle(`You were banned from **${interaction.guild.name}**.`)
-                .setDescription(reasonForBan)
-                .setColor('0xdf0000')
-                .setFooter({
-                    text: interaction.guild.name, 
-                    iconURL: interaction.guild.iconURL({ dynamic : true })
-                })
-                .setTimestamp()
-            
-            try {
-                await userToBan.send({ embeds: [banEmbed] })
-            } catch(err) {
-                console.log(err)
-            }
-                
-            const bannedEmbed = new EmbedBuilder()
-                .setDescription(`${userToBan} was banned.`)
-                .setColor('0x32ff25')
-            interaction.reply({ embeds: [bannedEmbed] })
-
-        } else {
-            const permsEmbed = new EmbedBuilder()
-                .setDescription('You do not have permission to use this command.')
-                .setColor('0xdf0000')
-            return interaction.reply({ embeds: [permsEmbed], ephemeral: true })
+        try {
+            await interaction.guild.members.ban(userToBan, options = { reason: reasonForBan});
+        } catch(err) {
+            return console.error(err);
         }
-	},
+
+        const logEmbed = new EmbedBuilder()
+            .setTitle(userToBan.tag + ' was banned.')
+            .addFields([
+                { name: 'User ID: ', value: `${userToBan.id}`},
+                { name: 'By: ', value: `${interaction.user}`},
+                { name: 'Reason: ', value: reasonForBan},
+            ])
+            .setColor('0xdf0000')
+            .setThumbnail(userToBan.displayAvatarURL({ dynamic : true }))
+            .setFooter({
+                text: interaction.guild.name, 
+                iconURL: interaction.guild.iconURL({ dynamic : true })
+            })
+            .setTimestamp();
+        modChannel.send({ embeds: [logEmbed] });
+
+        const banEmbed = new EmbedBuilder()
+            .setTitle(`You were banned from **${interaction.guild.name}**.`)
+            .setDescription(reasonForBan)
+            .setColor('0xdf0000')
+            .setFooter({
+                text: interaction.guild.name, 
+                iconURL: interaction.guild.iconURL({ dynamic : true })
+            })
+            .setTimestamp();
+        
+        try {
+            await userToBan.send({ embeds: [banEmbed] });
+        } catch(err) {
+            console.error(error);
+        }
+            
+        const bannedEmbed = new EmbedBuilder()
+            .setDescription(`${userToBan} was banned.`)
+            .setColor('0x32ff25');
+        interaction.reply({ embeds: [bannedEmbed] });
+    }
 }
