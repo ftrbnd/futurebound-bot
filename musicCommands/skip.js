c845de660e3097d2a96019395e339a9b73038e05
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-02-14 11:49:47 -0800

    Temp fix

--- musicCommands/skip.js
+++ musicCommands/skip.js
@@ -6,22 +6,22 @@ module.exports = {
 		.setDescription('Skip the current song in the queue'),
 		
 	async execute(interaction) {
-        const getAllowedRoleId = require('../helperFunctions/getAllowedRoleId');
-        const allowedRoleId = await getAllowedRoleId.execute(interaction);
-        if (!interaction.member._roles.includes(allowedRoleId) || allowedRoleId != interaction.guild.roles.everyone.id) {
-            const errEmbed = new EmbedBuilder()
-                .setDescription(`You do not have permission to use music commands right now!`)
-                .setColor('0xdf0000');
-            return interaction.reply({ embeds: [errEmbed] });
-        }
+        // const getAllowedRoleId = require('../helperFunctions/getAllowedRoleId');
+        // const allowedRoleId = await getAllowedRoleId.execute(interaction);
+        // if (!interaction.member._roles.includes(allowedRoleId) || allowedRoleId != interaction.guild.roles.everyone.id) {
+        //     const errEmbed = new EmbedBuilder()
+        //         .setDescription(`You do not have permission to use music commands right now!`)
+        //         .setColor('0xdf0000');
+        //     return interaction.reply({ embeds: [errEmbed] });
+        // }
 
-        const voiceChannel = interaction.member.voice.channel;
-        if(!voiceChannel) {
-            const errEmbed = new EmbedBuilder()
-                .setDescription(`You must join a voice channel!`)
-                .setColor('0xdf0000');
-            return interaction.reply({ embeds: [errEmbed] });
-        }
+        // const voiceChannel = interaction.member.voice.channel;
+        // if(!voiceChannel) {
+        //     const errEmbed = new EmbedBuilder()
+        //         .setDescription(`You must join a voice channel!`)
+        //         .setColor('0xdf0000');
+        //     return interaction.reply({ embeds: [errEmbed] });
+        // }
 
         const queue = interaction.client.DisTube.getQueue(interaction.guild);
         if(!queue) {
