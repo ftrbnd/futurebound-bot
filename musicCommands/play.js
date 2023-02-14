cf4393537ef2f09fe3241f7958f729f8b1ee5d5e
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-02-14 11:44:46 -0800

    Temp fix

--- musicCommands/play.js
+++ musicCommands/play.js
@@ -10,14 +10,14 @@ module.exports = {
             .setRequired(true)),
 		
 	async execute(interaction) {
-        const getAllowedRoleId = require('../helperFunctions/getAllowedRoleId');
-        const allowedRoleId = await getAllowedRoleId.execute(interaction);
-        if (!interaction.member._roles.includes(allowedRoleId) || allowedRoleId != interaction.guild.roles.everyone.id) {
-            const errEmbed = new EmbedBuilder()
-                .setDescription(`You do not have permission to use music commands right now!`)
-                .setColor('0xdf0000');
-            return interaction.reply({ embeds: [errEmbed] });
-        }
+        // const getAllowedRoleId = require('../helperFunctions/getAllowedRoleId');
+        // const allowedRoleId = await getAllowedRoleId.execute(interaction);
+        // if (!interaction.member._roles.includes(allowedRoleId) || allowedRoleId != interaction.guild.roles.everyone.id) {
+        //     const errEmbed = new EmbedBuilder()
+        //         .setDescription(`You do not have permission to use music commands right now!`)
+        //         .setColor('0xdf0000');
+        //     return interaction.reply({ embeds: [errEmbed] });
+        // }
 
         const voiceChannel = interaction.member.voice.channel;
         if(!voiceChannel) {
