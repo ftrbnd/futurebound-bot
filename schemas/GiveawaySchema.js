aced1c89c64aaf9978bd65cf8f9ddfe5cdb3a111
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-04-13 23:29:23 -0700

    Create giveaway schema

---
+++ schemas/GiveawaySchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const GiveawaySchema = new mongoose.Schema({
+    prize: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+    },
+    description: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+    },
+    endDate: {
+        type: mongoose.SchemaTypes.Date,
+        required: true,
+    },
+    entries: {
+        type: mongoose.SchemaTypes.Array,
+        required: false,
+    }
+}, { versionKey: false });
+
+module.exports = mongoose.model('Giveaway', GiveawaySchema);
