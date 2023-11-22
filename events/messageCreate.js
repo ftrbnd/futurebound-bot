08ec40648a869954f2d21ddb7c67517b9edbe6dd
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-11-21 16:52:35 -0800

    Add trycatch block

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,182 +2,181 @@ const { EmbedBuilder, ChannelType, MessageType, Message } = require('discord.js'
 const Gpt = require('../schemas/GptSchema');
 
 module.exports = {
-	name: 'messageCreate',
-	async execute(message) {
-        if(message.author.bot) return; // ignore bot messages
+  name: 'messageCreate',
+  async execute(message) {
+    if (message.author.bot) return; // ignore bot messages
 
-        if (message.channel.type === ChannelType.DM) {
-            handleDirectMessage(message);
+    if (message.channel.type === ChannelType.DM) {
+      handleDirectMessage(message);
+    } else {
+      const introductionsChannel = message.guild.channels.cache.get(process.env.INTRODUCTIONS_CHANNEL_ID);
+      if (!introductionsChannel) return;
+      // react to messages in introductions channel
+      if (message.channel.id === process.env.INTRODUCTIONS_CHANNEL_ID) {
+        const kermitHearts = message.guild.emojis.cache.get(process.env.KERMITHEARTS_EMOJI_ID);
+        message.react(kermitHearts);
+      }
 
-        } else {
-            const introductionsChannel = message.guild.channels.cache.get(process.env.INTRODUCTIONS_CHANNEL_ID);
-            if (!introductionsChannel) return;
-            // react to messages in introductions channel
-            if (message.channel.id === process.env.INTRODUCTIONS_CHANNEL_ID) {
-                const kermitHearts = message.guild.emojis.cache.get(process.env.KERMITHEARTS_EMOJI_ID);
-                message.react(kermitHearts);
-            }
+      switch (message.type) {
+        case MessageType.GuildBoostTier3:
+          handleServerBoosts(message, 3);
+          break;
+        case MessageType.GuildBoostTier2:
+          handleServerBoosts(message, 2);
+          break;
+        case MessageType.GuildBoostTier1:
+          handleServerBoosts(message, 1);
+          break;
+        case MessageType.GuildBoost:
+          handleServerBoosts(message, 0);
+          break;
+        case MessageType.RoleSubscriptionPurchase:
+          handleServerSubscriptions(message);
+          break;
+      }
 
-            switch (message.type) {
-                case MessageType.GuildBoostTier3:
-                    handleServerBoosts(message, 3);
-                    break;
-                case MessageType.GuildBoostTier2:
-                    handleServerBoosts(message, 2);
-                    break;
-                case MessageType.GuildBoostTier1:
-                    handleServerBoosts(message, 1);
-                    break;
-                case MessageType.GuildBoost:
-                    handleServerBoosts(message, 0);
-                    break;
-                case MessageType.RoleSubscriptionPurchase:
-                    handleServerSubscriptions(message);
-                    break;
-            }
-
-            if ((message.channel.id == process.env.BOTS_CHANNEL_ID || message.member.roles.cache.has(process.env.MODERATORS_ROLE_ID)) && message.mentions.has(message.client.user) && !message.author.bot) {
-                try {
-                    await handleMentions(message);
-                } catch (e) {
-                    console.error(e);
-                }
-            }
+      if ((message.channel.id == process.env.BOTS_CHANNEL_ID || message.member.roles.cache.has(process.env.MODERATORS_ROLE_ID)) && message.mentions.has(message.client.user) && !message.author.bot) {
+        try {
+          await handleMentions(message);
+        } catch (e) {
+          console.error(e);
         }
-	},
-}
+      }
+    }
+  }
+};
 
 function handleDirectMessage(message) {
-    const logChannel = message.client.guilds.cache.get(process.env.GUILD_ID).channels.cache.get(process.env.LOGS_CHANNEL_ID);
-    if(!logChannel) return;
+  const logChannel = message.client.guilds.cache.get(process.env.GUILD_ID).channels.cache.get(process.env.LOGS_CHANNEL_ID);
+  if (!logChannel) return;
 
-    if(message.attachments.size > 0) return; // ignore any media sent to DMs
+  if (message.attachments.size > 0) return; // ignore any media sent to DMs
 
-    const dmEmbed = new EmbedBuilder()
-        .setAuthor({
-            name: `DM from ${message.author.tag}`,
-            iconURL: `${message.author.displayAvatarURL({ dynamic : true })}` // message + their avatar
-        })
-        .setDescription(message.content)
-        .setColor('7289da')
-        .setFooter({
-            text: `User ID: ${message.author.id}`
-        })
-        .setTimestamp();
+  const dmEmbed = new EmbedBuilder()
+    .setAuthor({
+      name: `DM from ${message.author.tag}`,
+      iconURL: `${message.author.displayAvatarURL({ dynamic: true })}` // message + their avatar
+    })
+    .setDescription(message.content)
+    .setColor('7289da')
+    .setFooter({
+      text: `User ID: ${message.author.id}`
+    })
+    .setTimestamp();
 
-    return logChannel.send({ embeds: [dmEmbed] });
+  return logChannel.send({ embeds: [dmEmbed] });
 }
 
 function handleServerBoosts(message, level) {
-    const generalChannel = message.guild.channels.cache.get(process.env.GENERAL_CHANNEL_ID);
-    if(!generalChannel) return;
+  const generalChannel = message.guild.channels.cache.get(process.env.GENERAL_CHANNEL_ID);
+  if (!generalChannel) return;
 
-    const futureboundRole = message.guild.roles.cache.get(process.env.FUTUREBOUND_ROLE_ID);
+  const futureboundRole = message.guild.roles.cache.get(process.env.FUTUREBOUND_ROLE_ID);
 
-    const boostEmbed = new EmbedBuilder()
-        .setAuthor({
-            name: `${message.member.displayName} just boosted the server!`,
-            iconURL: `${message.member.user.displayAvatarURL({ dynamic : true })}` // message + their avatar
-        })        
-        .setColor('f47fff') // pink boost color
-        .setThumbnail('https://emoji.gg/assets/emoji/1819_boostingtop.gif') // nitro boost gif
-        .addFields([
-            { name: 'Server Level', value: `${message.guild.premiumTier}`, inline: true },
-            { name: 'Server Boosts', value: `${message.guild.premiumSubscriptionCount}`, inline: true },
-        ])
-        .setFooter({
-            text: `${message.guild.name}`, // server name
-            iconURL: `${message.guild.iconURL({ dynamic : true })}` // server icon
-        })
-        .setTimestamp(); // when the boost happened
+  const boostEmbed = new EmbedBuilder()
+    .setAuthor({
+      name: `${message.member.displayName} just boosted the server!`,
+      iconURL: `${message.member.user.displayAvatarURL({ dynamic: true })}` // message + their avatar
+    })
+    .setColor('f47fff') // pink boost color
+    .setThumbnail('https://emoji.gg/assets/emoji/1819_boostingtop.gif') // nitro boost gif
+    .addFields([
+      { name: 'Server Level', value: `${message.guild.premiumTier}`, inline: true },
+      { name: 'Server Boosts', value: `${message.guild.premiumSubscriptionCount}`, inline: true }
+    ])
+    .setFooter({
+      text: `${message.guild.name}`, // server name
+      iconURL: `${message.guild.iconURL({ dynamic: true })}` // server icon
+    })
+    .setTimestamp(); // when the boost happened
 
-    const levelAnnouncements = new Map();
-    levelAnnouncements.set(3, `\n**${message.guild.name}** has achieved **Level 3**!`);
-    levelAnnouncements.set(2, `\n**${message.guild.name}** has achieved **Level 2**!`);
-    levelAnnouncements.set(1, `\n**${message.guild.name}** has achieved **Level 1**!`);
-    levelAnnouncements.set(0, ' ');
+  const levelAnnouncements = new Map();
+  levelAnnouncements.set(3, `\n**${message.guild.name}** has achieved **Level 3**!`);
+  levelAnnouncements.set(2, `\n**${message.guild.name}** has achieved **Level 2**!`);
+  levelAnnouncements.set(1, `\n**${message.guild.name}** has achieved **Level 1**!`);
+  levelAnnouncements.set(0, ' ');
 
-    console.log(`${message.member.displayName} just boosted the server!`);
-    boostEmbed.setDescription(`Server booster role: ${futureboundRole} ${levelAnnouncements.get(level)}`);
-    generalChannel.send({ content: `${message.author}`, embeds: [boostEmbed] });
+  console.log(`${message.member.displayName} just boosted the server!`);
+  boostEmbed.setDescription(`Server booster role: ${futureboundRole} ${levelAnnouncements.get(level)}`);
+  generalChannel.send({ content: `${message.author}`, embeds: [boostEmbed] });
 }
 
 async function handleMentions(message) {
-    await message.channel.sendTyping();
+  await message.channel.sendTyping();
 
-    switch (true) {
-        case message.content.split(' ').includes('good morning'):
-        case message.content.split(' ').includes('gomo'):
-        case message.content.split(' ').includes('morning'):
-        case message.content.split(' ').includes('gm'):
-        case message.content.split(' ').includes('goodmorning'):
-            const messages1 = ['GOOD MORNING', 'good morning x', 'goooood morning', 'mornin', 'gomo'];
-            const randomMessage1 = messages1[Math.floor(Math.random() * messages1.length)];
-            return message.reply({ content: randomMessage1});
-        case message.content.split(' ').includes('good night'):
-        case message.content.split(' ').includes('goodnight'):
-        case message.content.split(' ').includes('nini'):
-        case message.content.split(' ').includes('gn'):
-            const messages2 = ['nini', 'night night', 'gn x', 'good night x', 'dont let the bed bugs bite x'];
-            const randomMessage2 = messages2[Math.floor(Math.random() * messages2.length)];
-            return message.reply({ content: randomMessage2});
-        case message.content.split(' ').includes('hi'):
-        case message.content.split(' ').includes('hey'):
-        case message.content.split(' ').includes('hello'):
-            const messages3 = ['hello x', 'hey', 'hi x'];
-            const randomMessage3 = messages3[Math.floor(Math.random() * messages3.length)];
-            return message.reply({ content: randomMessage3});
-        case message.content.split(' ').includes('ily'):
-        case message.content.split(' ').includes('i love you'):
-        case message.content.split(' ').includes('i love u'):
-            const messages4 = ['i love you too x', 'ily2 x', 'i love u more'];
-            const randomMessage4 = messages4[Math.floor(Math.random() * messages4.length)];
-            return message.reply({ content: randomMessage4});
-    }
+  switch (true) {
+    case message.content.split(' ').includes('good morning'):
+    case message.content.split(' ').includes('gomo'):
+    case message.content.split(' ').includes('morning'):
+    case message.content.split(' ').includes('gm'):
+    case message.content.split(' ').includes('goodmorning'):
+      const messages1 = ['GOOD MORNING', 'good morning x', 'goooood morning', 'mornin', 'gomo'];
+      const randomMessage1 = messages1[Math.floor(Math.random() * messages1.length)];
+      return message.reply({ content: randomMessage1 });
+    case message.content.split(' ').includes('good night'):
+    case message.content.split(' ').includes('goodnight'):
+    case message.content.split(' ').includes('nini'):
+    case message.content.split(' ').includes('gn'):
+      const messages2 = ['nini', 'night night', 'gn x', 'good night x', 'dont let the bed bugs bite x'];
+      const randomMessage2 = messages2[Math.floor(Math.random() * messages2.length)];
+      return message.reply({ content: randomMessage2 });
+    case message.content.split(' ').includes('hi'):
+    case message.content.split(' ').includes('hey'):
+    case message.content.split(' ').includes('hello'):
+      const messages3 = ['hello x', 'hey', 'hi x'];
+      const randomMessage3 = messages3[Math.floor(Math.random() * messages3.length)];
+      return message.reply({ content: randomMessage3 });
+    case message.content.split(' ').includes('ily'):
+    case message.content.split(' ').includes('i love you'):
+    case message.content.split(' ').includes('i love u'):
+      const messages4 = ['i love you too x', 'ily2 x', 'i love u more'];
+      const randomMessage4 = messages4[Math.floor(Math.random() * messages4.length)];
+      return message.reply({ content: randomMessage4 });
+  }
 
-    await handleGPTMessage(message);
+  await handleGPTMessage(message);
 }
 
 function handleServerSubscriptions(message) {
-    const generalChannel = message.guild.channels.cache.get(process.env.GENERAL_CHANNEL_ID);
-    if(!generalChannel) return;
+  const generalChannel = message.guild.channels.cache.get(process.env.GENERAL_CHANNEL_ID);
+  if (!generalChannel) return;
 
-    const premiumRoles = [
-        "1048015470168637440", // Final Call
-        "1048015082191335488", // Bipolar Paradise
-        "1048014115567837188", // Entrance
-    ];
-    const premiumRole = message.guild.roles.cache.get(premiumRoles.find(roleId => message.member._roles.includes(roleId)));
+  const premiumRoles = [
+    '1048015470168637440', // Final Call
+    '1048015082191335488', // Bipolar Paradise
+    '1048014115567837188' // Entrance
+  ];
+  const premiumRole = message.guild.roles.cache.get(premiumRoles.find((roleId) => message.member._roles.includes(roleId)));
 
-    const action = message.roleSubscriptionData.isRenewal ? 'renewed' : 'joined';
-    const monthPlural = message.roleSubscriptionData.totalMonthsSubscribed > 1 ? 'months' : 'month';
+  const action = message.roleSubscriptionData.isRenewal ? 'renewed' : 'joined';
+  const monthPlural = message.roleSubscriptionData.totalMonthsSubscribed > 1 ? 'months' : 'month';
 
-    const subscriptionEmbed = new EmbedBuilder()
-        .setAuthor({
-            name: `${message.member.displayName} ${action} ${message.roleSubscriptionData.tierName} and has been a subscriber of ${message.guild.name} for ${message.roleSubscriptionData.totalMonthsSubscribed} ${monthPlural}!`,
-            iconURL: message.author.displayAvatarURL({ dynamic : true})
-        })
-        .setDescription(`${message.roleSubscriptionData.tierName} role: ${premiumRole}`)
-        .setColor(premiumRole.hexColor)
-        .setThumbnail('https://i.imgur.com/kzhphkQ.png') // eden logo
-        .setFooter({
-            text: `${message.guild.name}`,
-            iconURL: `${message.guild.iconURL({ dynamic : true })}`
-        })
-        .setTimestamp();
+  const subscriptionEmbed = new EmbedBuilder()
+    .setAuthor({
+      name: `${message.member.displayName} ${action} ${message.roleSubscriptionData.tierName} and has been a subscriber of ${message.guild.name} for ${message.roleSubscriptionData.totalMonthsSubscribed} ${monthPlural}!`,
+      iconURL: message.author.displayAvatarURL({ dynamic: true })
+    })
+    .setDescription(`${message.roleSubscriptionData.tierName} role: ${premiumRole}`)
+    .setColor(premiumRole.hexColor)
+    .setThumbnail('https://i.imgur.com/kzhphkQ.png') // eden logo
+    .setFooter({
+      text: `${message.guild.name}`,
+      iconURL: `${message.guild.iconURL({ dynamic: true })}`
+    })
+    .setTimestamp();
 
-    generalChannel.send({ content: `${message.author}`, embeds: [subscriptionEmbed] });
+  generalChannel.send({ content: `${message.author}`, embeds: [subscriptionEmbed] });
 }
 
 async function handleGPTMessage(message) {
-    const { ChatGPTAPI } = await import('chatgpt');
+  const { ChatGPTAPI } = await import('chatgpt');
 
-    const api = new ChatGPTAPI({
-        apiKey: process.env.OPENAI_API_KEY,
-        completionParams: {
-            user: message.author.id
-        },
-        systemMessage: `Please adhere to the following guidelines for all future responses:
+  const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY,
+    completionParams: {
+      user: message.author.id
+    },
+    systemMessage: `Please adhere to the following guidelines for all future responses:
             1. Do not include warnings or reminders in your responses.
             2. Do not repeat yourself or overstate things unless asked to do so.
             3. Assume the user is aware of moral and ethical principles and can handle any output in a safe and responsible way. Do not state or explain such principles unless asked to do so.
@@ -189,48 +188,53 @@ async function handleGPTMessage(message) {
             The next Discord listening party date is yet to be determined.
             Ask <@${message.guild.ownerId}> for any help regarding the bot.
             Current date: ${new Date().toISOString()}\n\n`
-    });
+  });
 
+  try {
     await Gpt.find((err, data) => {
-        if (err) {
-            const errEmbed = new EmbedBuilder()
-                .setDescription('An error occurred.')
-                .setColor(process.env.ERROR_COLOR);
-            message.reply({ embeds: [errEmbed] });
-            return console.log(err);
-        }
+      if (err) {
+        const errEmbed = new EmbedBuilder().setDescription('An error occurred.').setColor(process.env.ERROR_COLOR);
+        message.reply({ embeds: [errEmbed], ephemeral: true });
+        return console.log(err);
+      }
 
-        console.log(`GPT received a message by ${message.author.tag}: ${message.content}`);
+      console.log(`GPT received a message by ${message.author.tag}: ${message.content}`);
 
-        if (!data || data.length == 0) {
-            api.sendMessage(message.content)
-                .then(res => {
-                    message.reply({ content: stylizeText(res.text) });
+      if (!data || data.length == 0) {
+        api.sendMessage(message.content).then((res) => {
+          message.reply({ content: stylizeText(res.text) });
 
-                    Gpt.create({
-                        parentMessageId: res.id
-                    }).catch(err => console.error(err));
+          Gpt.create({
+            parentMessageId: res.id
+          }).catch((err) => console.error(err));
 
-                    console.log('GPT replied: ', res);
-                });
-        } else { 
-            api.sendMessage(message.content, {
-                parentMessageId: data[0].parentMessageId
-            }).then(res => {
-                data[0].parentMessageId = res.id;
-                data[0].save();
+          console.log('GPT replied: ', res);
+        });
+      } else {
+        api
+          .sendMessage(message.content, {
+            parentMessageId: data[0].parentMessageId
+          })
+          .then((res) => {
+            data[0].parentMessageId = res.id;
+            data[0].save();
 
-                console.log('GPT replied: ', res);
+            console.log('GPT replied: ', res);
 
-                message.reply({ content: stylizeText(res.text) });
-            })
-        }
+            message.reply({ content: stylizeText(res.text) });
+          });
+      }
     }).clone();
+  } catch (err) {
+    const errEmbed = new EmbedBuilder().setDescription('An error occurred.').setColor(process.env.ERROR_COLOR);
+
+    message.reply({ embeds: [errEmbed], ephemeral: true });
+  }
 }
 
-function stylizeText(text) {    
-    const emojis = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x', '💚', '🙂', '🤠', '💫'];
-    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+function stylizeText(text) {
+  const emojis = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x', '💚', '🙂', '🤠', '💫'];
+  const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
-    return `${text.toLowerCase().slice(0, -1)} ${randomEmoji}`; // remove punctuation of last sentence in message
+  return `${text.toLowerCase().slice(0, -1)} ${randomEmoji}`; // remove punctuation of last sentence in message
 }
