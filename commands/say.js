904aa61ae07911bab37535a9e93200d9c4730e55
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-12-14 17:00:38 -0800

    Separate /say command into 2 subcommands

--- commands/say.js
+++ commands/say.js
@@ -5,46 +5,69 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('say')
     .setDescription('Make the bot say something')
-    .addChannelOption((option) =>
-      option.setName('channel').setDescription('The channel the message should be sent in').addChannelTypes(ChannelType.GuildAnnouncement, ChannelType.GuildText).setRequired(true)
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('message')
+        .setDescription('Send a quick message')
+        .addChannelOption((option) =>
+          option.setName('channel').setDescription('The channel the message should be sent in').addChannelTypes(ChannelType.GuildAnnouncement, ChannelType.GuildText).setRequired(true)
+        )
+        .addStringOption((option) => option.setName('message').setDescription('What the bot should say').setRequired(true))
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('announcement')
+        .setDescription('Allows for separate lines in a single message')
+        .addChannelOption((option) =>
+          option.setName('channel').setDescription('The channel the message should be sent in').addChannelTypes(ChannelType.GuildAnnouncement, ChannelType.GuildText).setRequired(true)
+        )
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator), // only the Server Moderator role can use this command
 
   async execute(interaction) {
+    const targetChannel = interaction.options.getChannel('channel');
+
     try {
-      const targetChannel = interaction.options.getChannel('channel');
+      if (interaction.options.getSubcommand() === 'message') {
+        const messageToSend = interaction.options.getString('message');
+        await targetChannel.send(messageToSend);
 
-      const sendEmbed = new EmbedBuilder().setDescription(`Enter the message that you want to announce into this channel within 3 minutes!`).setColor(process.env.CONFIRM_COLOR);
+        const sentEmbed = new EmbedBuilder().setDescription(`Said **"${messageToSend}"** in ${targetChannel}`).setColor(process.env.CONFIRM_COLOR);
 
-      await interaction.reply({ embeds: [sendEmbed] });
+        await interaction.reply({ embeds: [sentEmbed] });
+      } else if (interaction.options.getSubcommand() === 'announcement') {
+        const sendEmbed = new EmbedBuilder().setDescription(`Enter the message that you want to announce into this channel within 3 minutes!`).setColor(process.env.CONFIRM_COLOR);
 
-      const filter = (m) => m.author === interaction.user;
-      const collector = interaction.channel.createMessageCollector({ filter, time: 180000 }); // milliseconds
+        await interaction.reply({ embeds: [sendEmbed] });
 
-      collector.on('collect', async (m) => {
-        const announcementText = m.content;
+        const filter = (m) => m.author === interaction.user;
+        const collector = interaction.channel.createMessageCollector({ filter, time: 180000 }); // milliseconds
 
-        await targetChannel.send({ content: announcementText });
+        collector.on('collect', async (m) => {
+          const announcementText = m.content;
 
-        collector.stop();
-      });
+          await targetChannel.send({ content: announcementText });
 
-      collector.on('end', async (collected) => {
-        if (collected.size === 0) {
-          // if no message was entered
-          const couldntFindEmbed = new EmbedBuilder()
-            .setDescription(`You did not send a message within 3 minutes, please try again.`)
-            .setColor(process.env.ERROR_COLOR)
-            .setFooter({
-              text: interaction.guild.name,
-              iconURL: interaction.guild.iconURL({ dynamic: true })
-            });
-          await interaction.followUp({ embeds: [couldntFindEmbed], ephemeral: true });
-        } else {
-          const announcedEmbed = new EmbedBuilder().setDescription(`The announcement was sent!`).setColor(process.env.CONFIRM_COLOR);
-          await interaction.followUp({ embeds: [announcedEmbed] });
-        }
-      });
+          collector.stop();
+        });
+
+        collector.on('end', async (collected) => {
+          if (collected.size === 0) {
+            // if no message was entered
+            const couldntFindEmbed = new EmbedBuilder()
+              .setDescription(`You did not send a message within 3 minutes, please try again.`)
+              .setColor(process.env.ERROR_COLOR)
+              .setFooter({
+                text: interaction.guild.name,
+                iconURL: interaction.guild.iconURL({ dynamic: true })
+              });
+            await interaction.followUp({ embeds: [couldntFindEmbed], ephemeral: true });
+          } else {
+            const announcedEmbed = new EmbedBuilder().setDescription(`The announcement was sent!`).setColor(process.env.CONFIRM_COLOR);
+            await interaction.followUp({ embeds: [announcedEmbed] });
+          }
+        });
+      }
     } catch (err) {
       sendErrorEmbed(interaction, err);
     }
