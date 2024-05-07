95355c04b7e30a370b14edb87e2972625d4292a9
ftrbnd <giosalas25@gmail.com>
2024-05-07 15:46:40 -0700

    Remove callbacks from findOne() calls

--- commands/warn.js
+++ commands/warn.js
@@ -18,66 +18,63 @@ module.exports = {
       if (!modChannel) return;
 
       let warnCount;
-      await User.findOne({ discordId: userToWarn.id }, (err, data) => {
-        if (err) return console.error(err);
-
-        if (!data) {
-          // if the user isn't already in the database, add their data
-          User.create({
-            discordId: userToWarn.id,
-            username: userToWarn.username,
-            warnings: 1
-          }).catch((err) => console.log(err));
-          warnCount = 1;
+      const user = await User.findOne({ discordId: userToWarn.id });
+      if (!user) {
+        // if the user isn't already in the database, add their data
+        await User.create({
+          discordId: userToWarn.id,
+          username: userToWarn.username,
+          warnings: 1
+        });
+        warnCount = 1;
+      } else {
+        // if they already were in the database, simply update and save
+        if (!user.warnings) {
+          user.warnings = 1;
         } else {
-          // if they already were in the database, simply update and save
-          if (!data.warnings) {
-            data.warnings = 1;
-          } else {
-            data.warnings += 1;
-          }
-          data.username = userToWarn.username;
-          data.save();
-          warnCount = data.warnings;
+          user.warnings += 1;
         }
+        user.username = userToWarn.username;
+        await user.save();
+        warnCount = data.warnings;
+      }
 
-        const logEmbed = new EmbedBuilder()
-          .setTitle(userToWarn.tag + ' was warned.')
-          .addFields([
-            { name: 'User ID: ', value: `${userToWarn.id}` },
-            { name: 'By: ', value: `${interaction.user}` },
-            { name: 'Reason: ', value: reasonForWarn },
-            { name: 'Warnings: ', value: `${warnCount}` }
-          ])
-          .setColor('ffd100')
-          .setThumbnail(userToWarn.displayAvatarURL({ dynamic: true }))
-          .setFooter({
-            text: interaction.guild.name,
-            iconURL: interaction.guild.iconURL({ dynamic: true })
-          })
-          .setTimestamp();
-        modChannel.send({ embeds: [logEmbed] });
+      const logEmbed = new EmbedBuilder()
+        .setTitle(userToWarn.tag + ' was warned.')
+        .addFields([
+          { name: 'User ID: ', value: `${userToWarn.id}` },
+          { name: 'By: ', value: `${interaction.user}` },
+          { name: 'Reason: ', value: reasonForWarn },
+          { name: 'Warnings: ', value: `${warnCount}` }
+        ])
+        .setColor('ffd100')
+        .setThumbnail(userToWarn.displayAvatarURL({ dynamic: true }))
+        .setFooter({
+          text: interaction.guild.name,
+          iconURL: interaction.guild.iconURL({ dynamic: true })
+        })
+        .setTimestamp();
+      await modChannel.send({ embeds: [logEmbed] });
 
-        const warnEmbed = new EmbedBuilder()
-          .setTitle(`You were warned in **${interaction.guild.name}**.`)
-          .setDescription(reasonForWarn)
-          .addFields([{ name: 'Warnings: ', value: `${warnCount}` }])
-          .setColor('ffd100')
-          .setFooter({
-            text: interaction.guild.name,
-            iconURL: interaction.guild.iconURL({ dynamic: true })
-          })
-          .setTimestamp();
+      const warnEmbed = new EmbedBuilder()
+        .setTitle(`You were warned in **${interaction.guild.name}**.`)
+        .setDescription(reasonForWarn)
+        .addFields([{ name: 'Warnings: ', value: `${warnCount}` }])
+        .setColor('ffd100')
+        .setFooter({
+          text: interaction.guild.name,
+          iconURL: interaction.guild.iconURL({ dynamic: true })
+        })
+        .setTimestamp();
 
-        try {
-          userToWarn.send({ embeds: [warnEmbed] });
-        } catch (err) {
-          return console.error(err);
-        }
+      try {
+        await userToWarn.send({ embeds: [warnEmbed] });
+      } catch (err) {
+        return console.error(err);
+      }
 
-        const warnedEmbed = new EmbedBuilder().setDescription(`${userToWarn} was warned.`).setColor('ffd100');
-        interaction.reply({ embeds: [warnedEmbed] });
-      }).clone();
+      const warnedEmbed = new EmbedBuilder().setDescription(`${userToWarn} was warned.`).setColor('ffd100');
+      interaction.reply({ embeds: [warnedEmbed] });
     } catch (err) {
       sendErrorEmbed(interaction, err);
     }
