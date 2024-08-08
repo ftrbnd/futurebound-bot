bc5b08abec4bb336d58e7065c6b8848fe8499cab
ftrbnd <giosalas25@gmail.com>
2024-08-08 15:35:48 -0700

    Add ffmpeg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { env } from './utils/env.js';
+import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
+import ffmpeg from 'fluent-ffmpeg';
 import { client } from './bot.js';
 import { connectToDb } from './lib/mongo/index.js';
 import { registerHeardleJobs } from './lib/heardle/cron.js';
+
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 client.login(env.DISCORD_TOKEN);
 
