8896ce1aa6262b983b89f7baafb59708ca5fb615
ftrbnd <giosalas25@gmail.com>
2025-11-18 20:29:38 -0800

    Add deleteMessageSeconds ban option

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
 import { sendMessageInLogChannel } from '../utils/error-handler.js';
 import { env } from '../utils/env.js';
 import { Colors } from '../utils/constants.js';
@@ -8,16 +8,24 @@ export const data = new SlashCommandBuilder()
   .setDescription('Ban a user from the server')
   .addUserOption((option) => option.setName('user').setDescription('The user to be banned').setRequired(true))
   .addStringOption((option) => option.setName('reason').setDescription('The reason for the ban').setRequired(true))
+  .addNumberOption((option) => option.setName('days').setDescription('Number of days of messages to delete (1-7)').setMinValue(1).setMaxValue(7).setRequired(false))
   .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
 
+/**
+ * @param {ChatInputCommandInteraction} interaction
+ */
 export async function execute(interaction) {
   const userToBan = interaction.options.getUser('user');
   const reasonForBan = interaction.options.getString('reason');
+  const daysToDelete = interaction.options.getNumber('days') ?? 0;
 
   const modChannel = interaction.guild.channels.cache.get(env.MODERATORS_CHANNEL_ID);
   if (!modChannel) return;
 
-  await interaction.guild.members.ban(userToBan, { reason: reasonForBan });
+  await interaction.guild.members.ban(userToBan, {
+    reason: reasonForBan,
+    deleteMessageSeconds: daysToDelete * 24 * 60 * 60
+  });
 
   const logEmbed = new EmbedBuilder()
     .setTitle(userToBan.tag + ' was banned.')
